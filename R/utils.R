3b3dc8917bf7916d717de69baa09e9fd137c6e3f
OJWatson <oj.watson@hotmail.co.uk>
2023-04-04 18:08:21 +0100

    First commit with basic structure

---
+++ R/utils.R
@@ -0,0 +1,28 @@
+#' Save Figures
+#'
+#' @param name Name of figure
+#' @param fig ggplot or similar figure object
+#' @param width Width of plot in inches. Default = 6
+#' @param height Height of plot in inches. Default = 6
+#' @param plot_dir Plotting directory. Defaults to "analysis/plots"
+#' @importFrom grDevices dev.off pdf
+save_figs <- function(name,
+                      fig,
+                      width = 6,
+                      height = 6,
+                      plot_dir = file.path(here::here(), "analysis/plots")) {
+
+  dir.create(plot_dir, showWarnings = FALSE)
+  fig_path <- function(name) {paste0(plot_dir, "/", name)}
+
+  cowplot::save_plot(filename = fig_path(paste0(name,".png")),
+                     plot = fig,
+                     base_height = height,
+                     base_width = width,
+                     dpi = 600)
+
+  pdf(file = fig_path(paste0(name,".pdf")), width = width, height = height)
+  print(fig)
+  dev.off()
+
+}
